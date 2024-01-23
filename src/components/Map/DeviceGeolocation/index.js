34089c7b2050f90f2594527c1d3949e0d485bab9
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-01-23 13:49:53 -0500

    rotate device marker according to heading

--- src/components/Map/DeviceGeolocation/index.js
+++ src/components/Map/DeviceGeolocation/index.js
@@ -1,16 +1,38 @@
+import { useEffect } from "react"
 import { DivIcon } from "leaflet"
 import { Marker } from "react-leaflet"
+
+// TODO - handle hardcoding better
+function getIconTransform (degrees = 15) {
+    return `scale(0.8) translate(-14px, -14px) rotate(${degrees}deg)`
+}
 
 const icon = new DivIcon({
     className: "arrowhead",
     html: `
-        <svg xmlns="http://www.w3.org/2000/svg" style="transform: scale(0.8) translate(-14px, -14px) rotate(15deg)" height="40" width="40" viewBox="0 0 20 40">
+        <svg
+            id="device-marker"
+            xmlns="http://www.w3.org/2000/svg"
+            style="transform: ${getIconTransform()}"
+            height="40"
+            width="40"
+            viewBox="0 0 20 40"
+        >
             <polygon points="10,0 20,40 10,30 0,40" fill="#4285f4" stroke="#ffffff" stroke-width="2"/>
         </svg>
     `
 })
 
 function DeviceGeolocation({ geolocation }) {
+    useEffect(() => {
+        if (!geolocation) return
+
+        // This is hacky because: how else to rotate the marker in the map? ...
+        const svg = document.getElementById("device-marker")
+
+        svg.style.transform = getIconTransform(geolocation.heading)
+    }, [geolocation])
+
     if (!geolocation) return
 
     const position = {
--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -2,14 +2,21 @@ import { useEffect, useState } from "react";
 import usePersistentState from "./usePersistentState";
 import { GEOLOCATION_OPTION } from "../constants.js"
 
+const DEFAULT_HEADING = 15
+
 function useGeolocation() {
     const [enabled] = usePersistentState(GEOLOCATION_OPTION)
     const [position, setPosition] = useState()
     useEffect(() => {
         if (!enabled) return
 
-        const onSuccess = ({ coords }) => setPosition(coords)
-        navigator.geolocation.getCurrentPosition(onSuccess);
+        const onSuccess = ({ coords }) => {
+            const { latitude, longitude } = coords
+            const heading = coords.heading ?? DEFAULT_HEADING
+            setPosition({ latitude, longitude, heading })
+        }
+        const watcherId = navigator.geolocation.watchPosition(onSuccess);
+        return () => navigator.geolocation.clearWatch(watcherId)
     }, [enabled])
 
     return position
