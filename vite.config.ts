2a05fa10c1e0ba6de695f26a58b1f772293cd8d8
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-05-04 10:51:57 -0500

    add sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    sourcemap: 'inline'
+  },
   server: {
     port: 3000,
     proxy: {
