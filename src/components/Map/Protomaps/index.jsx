f2c4e5c34d68005082c25d2940ff6040e16e38f5
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-05-03 20:43:12 -0500

    upgrade protomaps to protomaps-leaflet

--- src/components/Map/Protomaps/index.jsx
+++ src/components/Map/Protomaps/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useMap } from 'react-leaflet'
 import { getMapTileHost, isAllowedToAccessMapTileHost } from "../../../utils.ts"
+import { leafletLayer } from 'protomaps-leaflet'
+import useScript from '../../../hooks/useScript'
 
 const INITIAL_CENTER = [19.476950206488414, -2.2851562500000004]
 const INITIAL_ZOOM = 2
@@ -14,13 +16,18 @@ function Protomaps (props) {
         map.on("moveend", () => onBoundsChange(map))
 
         const mapTileHost = getMapTileHost()
-        console.log({mapTileHost})
         if (!isAllowedToAccessMapTileHost(mapTileHost)) return
 
         const url = `${getMapTileHost()}/${file}`
-        var layer = window.protomaps.leafletLayer({ url })
+        var layer = leafletLayer({ url, theme: 'white' })
         layer.addTo(map)
     }, [map, file, onBoundsChange])
+
+    // This is kind of ugly. These libraries are nice but they depend on an old version of leaflet.
+    // They could be forked, or even installed with --legacy-peer-deps, or some other workaround.
+    // But here it is: this is the workaround that is used now :)
+    useScript("https://unpkg.com/leaflet-doubletapdrag")
+    useScript("https://unpkg.com/leaflet-doubletapdragzoom")
 
     return null
 }
