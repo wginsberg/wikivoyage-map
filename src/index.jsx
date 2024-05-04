4a319ec83bedc1716e759d294a288a572596bcb6
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-05-04 10:42:48 -0500

    cleanup dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import App from './App';
 import Settings from './Settings';
 import "./index.css"
-import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +16,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
