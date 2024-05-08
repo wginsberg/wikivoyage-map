bc6d96b675efcb3290c6f6dd2732ec1e0c86d611
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-05-08 10:51:24 -0500

    upgrade protomaps-leaflet to 3.1.1

--- src/components/Map/Protomaps/index.jsx
+++ src/components/Map/Protomaps/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useMap } from 'react-leaflet'
-import { getMapTileHost, isAllowedToAccessMapTileHost } from "../../../utils.ts"
+import { getMapTileHost } from "../../../utils.ts"
 import { leafletLayer } from 'protomaps-leaflet'
 
 const INITIAL_CENTER = [19.476950206488414, -2.2851562500000004]
@@ -15,7 +15,6 @@ function Protomaps (props) {
         map.on("moveend", () => onBoundsChange(map))
 
         const mapTileHost = getMapTileHost()
-        if (!isAllowedToAccessMapTileHost(mapTileHost)) return
 
         const url = `${getMapTileHost()}/${file}`
         var layer = leafletLayer({ url, theme: 'white' })
--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,14 @@
-const PROXY_URL = "/proxy"
 const CLOUDFLARE_BUCKET_URL = "https://pub-5ba95de8cc2f4dada22bfe563b284734.r2.dev"
-
-function isAllowedToAccessMapTileHost(host: string): boolean {
-    const isProxiedMapTileHost = host === PROXY_URL
-    if (isProxiedMapTileHost) return true
-
-    const isLocalhostEnvironment = window.location.host.split(":")[0] === 'localhost'
-    if (!isLocalhostEnvironment) return true
-
-    return false
-}
 
 function getMapTileHost() {
     // in development we proxy requests to cloudflare to avoid CORS
     // in production we make requests directly to cloudflare
     const host = process.env.NODE_ENV === "production"
         ? CLOUDFLARE_BUCKET_URL
-        : PROXY_URL
+        : `http://${window.location.host}/proxy`
     return host
 }
 
 export {
-    isAllowedToAccessMapTileHost,
     getMapTileHost
 }
