58cad78a7396a5580ae2427d843d2d2056215944
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-09-18 11:10:01 -0500

    fix svg styling for ios

--- app/src/components/Map/GeolocationButton/index.jsx
+++ app/src/components/Map/GeolocationButton/index.jsx
@@ -3,7 +3,7 @@ function GeolocationButton(props) {
 
     return (
         <button onClick={onClick} className="geolocation leaflet-bar">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="midnightblue" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <circle cx="12" cy="12" r="10" />
                 <circle cx="12" cy="12" r="6"/>
                 <line x1="12" y1="2" x2="12" y2="6" />
